e67c79160a767fd1f737d19b5f2777190ed2133d
laynekawahara <lkawahara@student.neumont.edu>
2015-10-17 15:53:13 -0600

    Enemy chases player.  Prefab created for enemy.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -6,57 +6,83 @@ using System;
 [RequireComponent(typeof(CharacterController))]
 public class Enemy : MonoBehaviour
 {
-	public int damage;
     public GameObject[] pathPoints;
-    private Vector3 fromPosition;
-    private Vector3 toPosition;
-    private CharacterController controller;
-    public bool moving;
     public float speed;
-    private int nextPointIndex;
+    public int damage;
+    public bool isPatrolling;
+    public float waitDuration;
+
+    private CharacterController controller;
+    private Vector3 fromPosition;
+    private Vector3 targetPosition;
+    private Vector3 lastSeenPlayerLocation; //when the player enters the FOV of enemy, this will be updated by enemyFOV script
+    private float yMovement = 0;
+    private float waitStart;
+    private int nextNodeIndex;
+    private bool isChasing;
+    private bool isWaiting;
 
     void Start() {
-        nextPointIndex = 0;
+        nextNodeIndex = 0;
+        waitStart = 0;
         controller = GetComponent<CharacterController>();
         if (pathPoints.Length > 0)
-            transform.position = pathPoints[nextPointIndex].transform.position;
+            transform.position = pathPoints[nextNodeIndex].transform.position;
 
         fromPosition = transform.position;
-        toPosition = transform.position;
+        targetPosition = transform.position;
         NextTarget();
+        isChasing = false;
+        isWaiting = false;
     }
 
-    private float yMovement = 0;
-    void Update() {
-        //if enemies have the ability to get swept up, don't move them in the air
-        if (moving) {
-
-            Vector3 path = toPosition - fromPosition;
-            Vector3 direction = Vector3.Normalize(path);
-            direction *= Time.deltaTime * speed;
-            float xMovement = direction.x * Time.deltaTime * speed;
-            float zMovement = direction.z * Time.deltaTime * speed;
-            
-            Vector3 moveVector = new Vector3(xMovement, yMovement, zMovement);
-            //transform.position += direction;// moveVector;
-            Debug.Log(moveVector);
-            controller.Move(moveVector);
-            //either at target or overshot target
-            if (transform.position == toPosition 
-                || Vector3.Magnitude(fromPosition - transform.position) > Vector3.Magnitude(path))
+    void Update() 
+    {
+        //waiting after reaching chase location
+        if (isWaiting) 
+        {
+            Vector3 path = new Vector3();
+            if ((System.DateTime.Now.Ticks * 10000) - waitStart > (waitDuration / 1000))
             {
+                isWaiting = false;
+                ChangeTarget(pathPoints[nextNodeIndex].transform.position);
+            }
+        } 
+        else if (isChasing) 
+        {
+            Vector3 path = targetPosition - fromPosition;
+            Vector3 direction = Vector3.Normalize(path) * Time.deltaTime * speed;
+            controller.Move(new Vector3(direction.x, yMovement, direction.z));
+            if (ReachedOrOverShotTarget(path, targetPosition))
+            {
+                isChasing = false;
+                isWaiting = true;
+                waitStart = System.DateTime.Now.Ticks * 10000;
+            }
+        }
+        //don't patrol if chasing
+        else if (isPatrolling)  {
+            Vector3 path = targetPosition - fromPosition;
+            Vector3 direction = Vector3.Normalize(path) * Time.deltaTime * speed;
+            controller.Move(new Vector3(direction.x, yMovement, direction.z));
+            if (ReachedOrOverShotTarget(path, targetPosition)){
                 NextTarget();
             }
         }
+        
     }
 
 
-    
+
+    bool ReachedOrOverShotTarget(Vector3 path, Vector3 target){
+        return (transform.position == target
+            || Vector3.Magnitude(fromPosition - transform.position) > Vector3.Magnitude(path));
+    }
     //auto change chase target to next point in path points []
     void NextTarget() {
         if (pathPoints.Length > 0) {
-            nextPointIndex = (nextPointIndex > pathPoints.Length - 1) ? 0 : nextPointIndex + 1;
-            ChangeTarget(pathPoints[nextPointIndex].transform.position);
+            nextNodeIndex = (nextNodeIndex >= pathPoints.Length - 1) ? 0 : nextNodeIndex + 1;
+            ChangeTarget(pathPoints[nextNodeIndex].transform.position);
         }
     }
 
@@ -64,8 +90,26 @@ public class Enemy : MonoBehaviour
     void ChangeTarget(Vector3 targetPos) {
         gameObject.transform.LookAt(targetPos);
         fromPosition = transform.position;
-        toPosition = targetPos;
+        targetPosition = targetPos;
+    }
+
+    public void ChasePlayer(Collider player) {
+        isChasing = true;
+        ChangeTarget(player.transform.position);
     }
 }
 
 
+//enemy patrols around nodes
+//TODO if player is within fov range, will update lastSeenPosition
+    //TODO fov will continue to check if player is in range depending on enemy's reaction time
+        //TODO slower reactions will result in a less constant check
+//TODO enemy will lerp towards lastSeenPosition 
+    //damage player on collision
+    //TODO ensure enemy collider object also has enemy tag
+//TODO Once enemy reaches the last seen position
+    //TODO if player is no longer in range
+        //TODO wait for a moment
+        //TODO then go back to patrolling (resume targeting last node target)
+    //FOV TODO if player is still in range
+        //TODO reset fov reaction timer
---
+++ Assets/Scripts/Enemies/EnemyFieldOfView.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFieldOfView : MonoBehaviour {
+
+    private Enemy attachedEnemy;
+
+	void Start () {
+        attachedEnemy = GetComponentInParent<Enemy>();
+        if (!attachedEnemy) {
+            Debug.LogError("This field of view doesn't have a parent with an enemy script; fov name: " + name);
+        }
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        UpdatePlayerLastKnownPosition(col);
+    }
+
+    void OnTriggerStay(Collider col) {
+        UpdatePlayerLastKnownPosition(col);
+    }
+
+    void UpdatePlayerLastKnownPosition(Collider col)
+    {
+        if ( col.gameObject.CompareTag("Player") ){
+             Debug.Log("Enemy sees player!!");
+             attachedEnemy.ChasePlayer(col);
+        }
+    }
+}
