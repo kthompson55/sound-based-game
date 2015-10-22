12ada6fbbf044930c2f8960b17f187b0639dc5d4
Karl Thompson <karlthompsonnu@gmail.com>
2015-10-22 08:42:13 -0600

    Slow mid-air movement changes

--- Assets/Scripts/Character/PhysicalBodyLocal.cs
+++ Assets/Scripts/Character/PhysicalBodyLocal.cs
@@ -13,11 +13,13 @@ public class PhysicalBodyLocal : NetworkBehaviour
     public float jumpHeight;
     public float jumpSpeed;
     public float tapJumpTime;
+    public float jumpMovementModifier;
 
     private CharacterController controller;
     private Rigidbody rigidbody;
     private bool jumping;
     private float jumpTracking;
+    private Vector3 jumpingMovementDirection;
 
     void Start()
     {
@@ -57,6 +59,23 @@ public class PhysicalBodyLocal : NetworkBehaviour
         otherCamera.SetActive(false);
 #endregion
 
+        float xMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zMovement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        Vector3 currentMovementNormal = new Vector3(xMovement, 0, zMovement).normalized;
+        // reduce movement speed if attempting to move in a direction different from your running direction prior to jumping
+        if(!controller.isGrounded)
+        {
+            Vector3 directionNormal = jumpingMovementDirection.normalized;
+            float totalDifferenceFromDirection = Mathf.Abs(currentMovementNormal.x - directionNormal.x) + Mathf.Abs(currentMovementNormal.z - directionNormal.z);
+            if (totalDifferenceFromDirection > .3f)
+            {
+                Debug.Log("movement reduction from jumping");
+                xMovement *= jumpMovementModifier;
+                zMovement *= jumpMovementModifier;
+            }
+        }
+
+        // handle upward and downward movements of jumping
         float yMovement = 0;
         if (controller.isGrounded && !jumping)
         {
@@ -65,6 +84,7 @@ public class PhysicalBodyLocal : NetworkBehaviour
                 Vector3 jumpForce = new Vector3(0, jumpHeight, 0);
                 jumpTracking = 0;
                 jumping = true;
+                jumpingMovementDirection = new Vector3(xMovement, 0, zMovement);
                 StartCoroutine("VerifyFullJump");
             }
         }
@@ -82,15 +102,16 @@ public class PhysicalBodyLocal : NetworkBehaviour
         {
             yMovement = Physics.gravity.y * Time.deltaTime;
         }
-        float xMovement = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
-        float zMovement = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
+        // change rotation based on current camera angle
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, followingCamera.transform.localEulerAngles.y, transform.localEulerAngles.z);
         Quaternion cameraRotation = Quaternion.Euler(transform.localEulerAngles);
+        // apply movement
         Vector3 moveVector = new Vector3(xMovement, yMovement, zMovement);
         controller.Move(cameraRotation * moveVector);
     }
 
+    // Is the jump button being tapped or held?
     IEnumerator VerifyFullJump()
     {
         yield return new WaitForSeconds(tapJumpTime);
