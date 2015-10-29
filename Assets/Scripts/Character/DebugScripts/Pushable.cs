c6669384c3a2a15b90a87f1896ed9fe1c5d24952
Vincent Malmrose <genundoryu@gmail.com>
2015-10-29 07:57:16 -0600

    Testing spirit player pushing things around.

---
+++ Assets/Scripts/Character/DebugScripts/Pushable.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Pushable : MonoBehaviour {
+    Vector3 pushDirection;
+    float pushTime;
+    bool pushing = false;
+
+    void Update()
+    {
+        if (pushing)
+        {
+            Debug.Log("Pushing is true");
+            pushing = false;
+            transform.position += pushDirection;
+            if ((System.DateTime.Now.Ticks * 10000) - pushTime > (50000 / 1000))
+            {
+                Debug.Log("Stop moving");
+            }
+        }
+
+    }
+    void OnTriggerEnter(Collider spirit)
+    {
+        if (spirit.GetComponent<SpiritualBody>() != null)
+        {
+            Debug.Log("Pushing");
+            pushTime = System.DateTime.Now.Ticks * 10000;
+            pushDirection = Vector3.Normalize(transform.position - spirit.transform.position);
+            pushing = true;
+        }
+    }
+}
