1761264216409cd4ec6a65de12b9f143ef3b5ec6
Tim Gates <sly12391237@gmail.com>
2015-11-04 08:16:37 -0700

    Start working on the Multiplayer lobby

--- Assets/Scripts/Networking/StartGameUI.cs
+++ Assets/Scripts/Networking/StartGameUI.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class StartGameUI : MonoBehaviour {
 
 
     public void MyOnClick()
-    {
+    {   
         GameObject.Find("NetworkManager").GetComponent<MyNetworkManager>().StartGame();
+    }
+
+    public void CreateGame(Text matchName)
+    {
+        MyNetworkManager temp=GameObject.Find("NetworkManager").GetComponent<MyNetworkManager>();
+        temp.matchName = matchName.text;
+        temp.matchSize = 2;
+        temp.StartGame();
     }
 
 }
