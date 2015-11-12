f021de8398f5b2f8f840183bbbacda597979d181
Karl Thompson <karlthompsonnu@gmail.com>
2015-11-12 08:42:42 -0700

    Menu can be navigated by keyboard/controller

---
+++ Assets/Scripts/Menu/ControllerEnabledMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Canvas))]
+public class ControllerEnabledMenu : MonoBehaviour 
+{
+    public Color unselectedButtonTextColor;
+    public Color selectedButtonTextColor;
+    
+    private List<Button> menuButtons;
+    private int currentButtonIndex;
+
+	// Use this for initialization
+	void Start () 
+    {
+        menuButtons = new List<Button>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Button currentChildButton = transform.GetChild(i).GetComponent<Button>();
+            if(currentChildButton)
+            {
+                menuButtons.Add(currentChildButton);
+            }
+        }
+
+        currentButtonIndex = 0;
+        menuButtons[0].GetComponentInChildren<Text>().color = selectedButtonTextColor;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        float direction = Input.GetAxis("Vertical");
+        menuButtons[currentButtonIndex].GetComponentInChildren<Text>().color = unselectedButtonTextColor;
+        // move up in menu
+        if(direction > 0 && currentButtonIndex > 0)
+        {
+            currentButtonIndex--;
+        }
+        // move down in menu
+        else if(direction < 0 && currentButtonIndex < menuButtons.Count - 1)
+        {
+            currentButtonIndex++;
+        }
+        menuButtons[currentButtonIndex].GetComponentInChildren<Text>().color = selectedButtonTextColor;
+	}
+}
