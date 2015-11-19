1b0d5e656a35a658b47f2a898d4f19782ace74e9
Karl Thompson <karlthompsonnu@gmail.com>
2015-11-19 14:17:37 -0700

    Add red flash when taking damage

--- Assets/Scripts/Character/Health.cs
+++ Assets/Scripts/Character/Health.cs
@@ -10,16 +10,23 @@ public class Health : MonoBehaviour
 	public int currHealth;
     private BoxCollider collisionBox;
     public HealthBar hpBar;
+    public Image damageSplash;
+    public Color flashColor;
+    public float flashSpeed;
 	
 	public float invincibilityTime = 0.5f;
 	private bool hit;
 	private long hitTime;
+    private bool flashOn;
+    private bool flashOff;
 	
 	void Start ()
 	{
 		currHealth = maxHealth;
 		collisionBox = GetComponent<BoxCollider> ();
 		hit = false;
+        flashOn = false;
+        flashOff = false;
         hpBar.SetHealth(GetComponent<Health>());
 	}
 
@@ -33,6 +40,15 @@ public class Health : MonoBehaviour
                 hit = false;
             }
         }
+        if(flashOn)
+        {
+            ShowDamageFlash();
+        }
+        else if(flashOff)
+        {
+            HideDamageFlash();
+        }
+
         if(currHealth <= 0)
         {
             //finish game
@@ -48,11 +64,11 @@ public class Health : MonoBehaviour
 	void OnTriggerEnter (Collider col)
 	{
 		if ((col.gameObject.tag == "Enemy" || col.gameObject.tag == "Trap") && !hit) {
-            Debug.Log("ouch");
             currHealth -= col.gameObject.tag == "Enemy" ? col.gameObject.GetComponent<Enemy> ().damage : col.gameObject.GetComponent<Trap> ().damage;
             currHealth = Mathf.Clamp(currHealth, 0, maxHealth);
             hit = true;
 			hitTime = System.DateTime.Now.Ticks * 10000;
+            flashOn = true;
 		}
 	}
 	
@@ -60,4 +76,33 @@ public class Health : MonoBehaviour
 	{
         return ((float)currHealth) / ((float)maxHealth);
 	}
+
+    void ShowDamageFlash()
+    {
+        damageSplash.color = Color.Lerp(damageSplash.color, flashColor, flashSpeed);
+        if(CompareColors(damageSplash.color, flashColor))
+        {
+            flashOn = false;
+            flashOff = true;
+        }
+    }
+
+    void HideDamageFlash()
+    {
+        damageSplash.color = Color.Lerp(damageSplash.color, Color.clear, flashSpeed);
+        if(CompareColors(damageSplash.color, Color.clear))
+        {
+            flashOff = false;
+        }
+    }
+
+    private bool CompareColors(Color first, Color second)
+    {
+        bool redValue = Mathf.Abs(first.r - second.r) < .05f;
+        bool greenValue = Mathf.Abs(first.g - second.g) < .05f;
+        bool blueValue = Mathf.Abs(first.b - second.b) < .05f;
+        bool alphaValue = Mathf.Abs(first.a - second.a) < .1f;
+
+        return redValue && greenValue && blueValue && alphaValue;
+    }
 }
