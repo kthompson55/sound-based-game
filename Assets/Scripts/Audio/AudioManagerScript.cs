ef4048cd3c16f051647bd282f5fdd3bab3af1277
Vincent Malmrose <genundoryu@gmail.com>
2015-11-24 11:14:28 -0700

    Audio first iteration
    How audio works currently.
    1. drag and drop the AudioManager prefab into a scene. And attach the
    physical and spiritual cameras to it. The manager uses whichever camera
    is enabled to tell if it should spawn a spirit or physical echo.
    
    2. Add the AudioSpawn script to any object that should spawn sounds.
    
    3. Fill in the clip and transform variables in the AudioSpawn script
    
    4. Make a script or edit a script to call the spawn function in
    AudioSpawn. For example, if you wanted to spawn a stalagtite dripping
    you would need to make a stalagtiteDrip script that will call spawn
    every 12.5 seconds. You can pass in a variable to say how long the sound
    should stay, otherwise it stays for 10 seconds.

---
+++ Assets/Scripts/Audio/AudioManagerScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AudioManagerScript : MonoBehaviour {
+    public Camera Physical;
+    public Camera Spiritual;
+    public GameObject mySoundPrefab;
+    public List<GameObject> soundList = new List<GameObject>(20);
+
+	void Start () {
+    }
+
+	void Update () {
+        foreach (GameObject s in soundList)
+        {
+            MySoundScript script = s.GetComponent<MySoundScript>();
+            if (script.time > script.endTime)
+            {
+                soundList.Remove(s);
+                Destroy(s);
+            }
+        }
+    }
+
+    public void Spawn(AudioClip clip, Transform position, float timeToPlay)
+    {
+        Debug.Log("Manager Spawning");
+        if (Physical.enabled)
+        {
+            physicalSpawn(position, clip, timeToPlay);
+        }
+        else if (Spiritual.enabled)
+        {
+            spiritSpawn(position, clip, timeToPlay);
+        }
+    }
+
+    private void spiritSpawn(Transform position, AudioClip clip, float timeToPlay)
+    {
+        Debug.Log("Spirit Spawn");
+        GameObject sound = Instantiate(mySoundPrefab);
+        AudioSource soundSource = sound.AddComponent<AudioSource>();
+        soundSource.transform.position = position.position;
+        soundSource.spatialBlend = 1.0f;
+        soundSource.rolloffMode = AudioRolloffMode.Linear;
+        soundSource.maxDistance = 100.0f;
+        soundSource.PlayOneShot(clip);
+        soundList.Add(sound);
+    }
+
+    private void physicalSpawn(Transform position, AudioClip clip, float timeToPlay)
+    {
+        Debug.Log("Physical Spawn");
+
+        GameObject sound = Instantiate(mySoundPrefab);
+
+        AudioSource soundSource = sound.GetComponent<AudioSource>();
+
+        //Cave like echos
+        AudioReverbFilter filter = sound.AddComponent<AudioReverbFilter>();
+        filter.reverbPreset = AudioReverbPreset.Cave;
+
+        soundSource.transform.position = position.position;
+        soundSource.spatialBlend = 1.0f;
+        soundSource.rolloffMode = AudioRolloffMode.Linear;
+        soundSource.maxDistance = 5.0f;
+        soundSource.volume = 0.25f;
+        soundSource.clip = clip;
+        soundSource.Play();
+        //soundSource.PlayOneShot(clip);
+        soundList.Add(sound);
+    }
+
+
+}
---
+++ Assets/Scripts/Audio/AudioSpawn.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AudioSpawn : MonoBehaviour {
+    public AudioClip clip;
+    public Transform position;
+    public AudioManagerScript manage;
+
+	// Use this for initialization
+	void Start () {
+        manage = GameObject.Find("AudioManager").GetComponent<AudioManagerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void spawn(float TimeToEnd = 10.0f)
+    {
+        Debug.Log("Spawner Spawning");
+        if (manage == null)
+        {
+            manage = GameObject.Find("AudioManager").GetComponent<AudioManagerScript>();
+        }
+        manage.Spawn(clip, position, TimeToEnd);
+    }
+}
